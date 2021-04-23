c4ecfb9827b6a21efc883e7c586229a9827d97c9
alextran0899 <alextran@MacBook-Air.local>
2021-04-23 16:53:01 -0500

    done

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -49,7 +49,7 @@ test('handleSelect is called when an season is selected', () => {
 });
 
 test('component renders when no seasons are selected and when rerenders with a season passed in', () => {
-
+    
     const { rerender } = render(<Show show={testShow} selectedSeason={"none"} />);
 	let episodes = screen.queryByTestId("episodes-container");
 	expect(episodes).toBeFalsy();
